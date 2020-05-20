95c5ddf2bc65647bc99a7f3419c5ddc2a36b837f
Giuliano Decesares <giulianodecesares@gmail.com>
2020-05-20 01:26:39 -0300

    Final details

--- Source/BatteryCollector/BatteryMan_GameMode.cpp
+++ Source/BatteryCollector/BatteryMan_GameMode.cpp
@@ -3,3 +3,30 @@
 
 #include "BatteryMan_GameMode.h"
 
+ABatteryMan_GameMode::ABatteryMan_GameMode()
+{
+    this->PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABatteryMan_GameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    FTimerHandle handle;
+    GetWorldTimerManager().SetTimer(handle, this, &ABatteryMan_GameMode::SpawnPlayerRecharge, FMath::RandRange(2.0f, 5.0f), true);
+    
+}
+
+void ABatteryMan_GameMode::Tick(float DeltaTme)
+{
+    Super::Tick(DeltaTme);
+}
+
+void ABatteryMan_GameMode::SpawnPlayerRecharge()
+{
+    float randX = FMath::RandRange(this->SpawnXMaximum, this->SpawnXMinimum);
+    float randY = FMath::RandRange(this->SpawnYMaximum, this->SpawnYMinimum);
+
+    FVector spawnPosition = FVector(randX, randY, this->spawnZ);
+    GetWorld()->SpawnActor(this->PlayerRecharge, &spawnPosition, &FRotator::ZeroRotator);
+}
--- Source/BatteryCollector/BatteryMan_GameMode.h
+++ Source/BatteryCollector/BatteryMan_GameMode.h
@@ -13,5 +13,28 @@ UCLASS()
 class BATTERYCOLLECTOR_API ABatteryMan_GameMode : public AGameMode
 {
 	GENERATED_BODY()
+
+	ABatteryMan_GameMode();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTme) override;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<APawn> PlayerRecharge;
+
+	float spawnZ = 130.0f;
 	
+	UPROPERTY(EditAnywhere)
+	float SpawnXMinimum;
+
+	UPROPERTY(EditAnywhere)
+    float SpawnXMaximum;
+
+	UPROPERTY(EditAnywhere)
+    float SpawnYMinimum;
+
+	UPROPERTY(EditAnywhere)
+    float SpawnYMaximum;
+
+	void SpawnPlayerRecharge();
 };
